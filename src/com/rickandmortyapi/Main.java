3172bc4fadd5f35f41d7f817b3ebca6765fbfbe8
Gustavo Goularte <goulartegustavo20@gmail.com>
2024-04-04 19:38:10 -0300

    "V2"

--- src/com/rickandmortyapi/Main.java
+++ src/com/rickandmortyapi/Main.java
@@ -1,49 +1,154 @@
 package com.rickandmortyapi;
 
 import com.rickandmortyapi.model.Apis;
+import com.rickandmortyapi.model.Metodos;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        int opcao;
         int idPersonagem = 0;
         Scanner ler = new Scanner(System.in);
         Random random = new Random();
         var apis = new Apis();
-            System.out.println("Digite um número entre 1 e 826: ");
-            idPersonagem = ler.nextInt();
-            int forcaPerso = random.nextInt(100);
-            System.out.println(apis.personagem(String.valueOf(idPersonagem)));
-            System.out.println("-- Sua força é de: " + forcaPerso);
-            System.out.println("----------------------------------------------------------");
+        System.out.println("Digite um número entre 1 e 826: ");
+        idPersonagem = ler.nextInt();
+        int forcaPerso = random.nextInt(100);
+        System.out.println(apis.personagem(String.valueOf(idPersonagem)));
+        System.out.println("-- Sua força é de: " + forcaPerso);
+        System.out.println("----------------------------------------------------------");
 
-            int numeroAleatorio = random.nextInt(826);
-            int lugar = random.nextInt(126);
+        int numeroAleatorio = random.nextInt(826);
+        int lugar = random.nextInt(126);
 
-            System.out.println("Seu adversário será: ");
-            System.out.println(apis.personagem(String.valueOf((numeroAleatorio))));
-            System.out.println("Força secreta");
-            int forcaInimigo = random.nextInt(95);
-            System.out.println("----------------------------------------------------------");
+        System.out.println("Seu adversário será: ");
+        System.out.println(apis.personagem(String.valueOf((numeroAleatorio))));
+        System.out.println("Força secreta");
+        int forcaInimigo = random.nextInt(95);
+        System.out.println("----------------------------------------------------------");
         System.out.println("Sua batalha sera aqui: ");
         System.out.println(apis.lugar(String.valueOf((lugar))));
         System.out.println("----------------------------------------------------------");
-            System.out.println("Deseja batalhar: ");
+        System.out.println("Deseja Treinar antes da batalhar? ");
+        System.out.println("1- Sim");
+        System.out.println("2- Não");
+        int resposta = ler.nextInt();
+        int forcaAdiconal = random.nextInt(30);
+        int poderPerdido = random.nextInt(30);
+        do {
+            System.out.println("----------------------------------------------------------");
+            System.out.println("Escolha um numero (1-28) para definir sua sorte no treino: ");
+            int opcaoTreino = ler.nextInt();
+            Metodos metodos = new Metodos();
+            if (opcaoTreino == 1) {
+                metodos.jerry(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 2) {
+                metodos.morty(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 3) {
+                metodos.snuffles(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 4) {
+                metodos.rickMulti(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 5) {
+                metodos.rick(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 6) {
+                metodos.treino(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 7) {
+                metodos.meeseeksFalha(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 8) {
+                metodos.entidadeEngana(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 9) {
+                metodos.portalCronenbergs(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 10) {
+                metodos.jerryDestroi(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 11) {
+                metodos.naveAvaria(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 12) {
+                metodos.gazorpazorpianosFuriosos(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 13) {
+                metodos.birdperson(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 14) {
+                metodos.meeseeksSucesso(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 15) {
+                metodos.unity(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 16) {
+                metodos.councilOfRicks(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 17) {
+                metodos.interdimensionalTrap(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 18) {
+                metodos.zigerionScam(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 19) {
+                metodos.portalMalFuncionamento(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 20) {
+                metodos.jerryInterrupts(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 21) {
+                metodos.interdimensionalAlliance(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 22) {
+                metodos.intergalacticChampion(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 23) {
+                metodos.ancientArtifact(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 24) {
+                metodos.meeseeksHelp(forcaAdiconal);
+                forcaPerso += forcaAdiconal;
+            } else if (opcaoTreino == 25) {
+                metodos.realityCronenberg(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 26) {
+                metodos.federationCapture(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 27) {
+                metodos.plutonianScam(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else if (opcaoTreino == 28) {
+                metodos.toxicDimension(poderPerdido);
+                forcaPerso -= poderPerdido;
+            } else {
+                System.out.println("Opção inválida");
+            }
+
+            System.out.println("Força atual: " + forcaPerso);
+
+            System.out.println("----------------------------------------------------------");
+            System.out.println("Deseja batalhar? ");
             System.out.println("1- Sim");
             System.out.println("2- Não");
-            int resposta = ler.nextInt();
+             opcao = ler.nextInt();
 
-            if (resposta == 1) {
+            if (opcao == 1) {
                 if (forcaPerso >= forcaInimigo) {
                     System.out.println("Voce é o ganhador e o novo chefe dessa dimensão, Parabens!");
                 } else {
-                    System.out.println("Voce perdeu,a força do oponente era de " + forcaInimigo+ " ,tente novamente");
+                    System.out.println("Voce perdeu,a força do oponente era de " + forcaInimigo + " ,tente novamente");
                 }
-            } else if (resposta == 2) {
-                System.out.println("Voce iria perder mesmo...");
+            } else if (opcao == 2) {
+                System.out.println("Vamos tentar novamente....");
             } else {
                 System.out.println("Resposta errada");
             }
-        }
+
+        }while (opcao == 2) ;
     }
+}
---
+++ src/com/rickandmortyapi/model/Metodos.java
@@ -0,0 +1,176 @@
+package com.rickandmortyapi.model;
+public class Metodos {
+    public void jerry (int forcaAdiconal){
+        System.out.println("Jerry tenta te sabotar, mas você o supera e ganha "+ forcaAdiconal+" de poder!");
+    }
+    public void morty (int forcaAdiconal){
+        System.out.println("Você encontra um Morty perdido, ao ajudá-lo você ganha "+forcaAdiconal+" de poder!");
+    }
+    public void snuffles (int forcaAdiconal){
+        System.out.println("VVocê faz amizade com um Snuffles superinteligente e ganha +30 de poder!");
+    }
+    public void rickMulti (int forcaAdiconal){
+        System.out.println("Um Rick do multiverso te ensina uma nova técnica, ganhe "+forcaAdiconal+" de poder!");
+    }
+    public void rick (int forcaAdiconal){
+        System.out.println("Rick te presenteia com uma arma intergaláctica, ganhe "+forcaAdiconal+" de poder!");
+    }
+    public void treino (int forcaAdiconal){
+        System.out.println("Você participa de um torneio intergaláctico e ganha " +forcaAdiconal+" de poder pela vitória!");
+    }
+    public void meeseeksFalha(int poderPerdido) {
+            System.out.println("Um Meeseeks falha em uma tarefa importante e você perde " + poderPerdido + " de poder!");
+        }
+        public void entidadeEngana(int poderPerdido) {
+            System.out.println("Uma entidade interdimensional te engana e você perde " + poderPerdido + " de poder!");
+        }
+
+        public void portalCronenbergs(int poderPerdido) {
+            System.out.println("Um portal de Cronenbergs se abre e você perde " + poderPerdido + " de poder na batalha!");
+        }
+
+        public void jerryDestroi(int poderPerdido) {
+            System.out.println("Jerry destrói uma invenção valiosa e você perde " + poderPerdido + " de poder!");
+        }
+
+        public void naveAvaria(int poderPerdido) {
+            System.out.println("Uma nave espacial avaria e você perde " + poderPerdido + " de poder!");
+        }
+
+        public void gazorpazorpianosFuriosos(int poderPerdido) {
+            System.out.println("Você é atacado por Gazorpazorpianos furiosos e perde " + poderPerdido + " de poder!");
+        }
+    public void birdperson(int forcaAdiconal) {
+        System.out.println("Você recebe a bênção de Birdperson e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void meeseeksSucesso(int forcaAdiconal) {
+        System.out.println("Um Meeseeks realiza uma tarefa crucial e você ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void unity(int forcaAdiconal) {
+        System.out.println("Unity se une a você e aumenta sua força em " + forcaAdiconal + " pontos!");
+    }
+
+    public void councilOfRicks(int forcaAdiconal) {
+        System.out.println("O Conselho dos Ricks te reconhece e te fortalece com " + forcaAdiconal + " de poder!");
+    }
+
+
+    public void interdimensionalTrap(int poderPerdido) {
+        System.out.println("Você cai em uma armadilha interdimensional e perde " + poderPerdido + " de poder!");
+    }
+
+    public void zigerionScam(int poderPerdido) {
+        System.out.println("Os Zigerions te enganam em um golpe e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void portalMalFuncionamento(int poderPerdido) {
+        System.out.println("Um portal mal funciona e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void jerryInterrupts(int poderPerdido) {
+        System.out.println("Jerry interrompe seu treino e você perde " + poderPerdido + " de poder!");
+    }
+
+
+    public void interdimensionalAlliance(int forcaAdiconal) {
+        System.out.println("Você forma uma aliança interdimensional e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void intergalacticChampion(int forcaAdiconal) {
+        System.out.println("Você se torna o campeão intergaláctico e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void ancientArtifact(int forcaAdiconal) {
+        System.out.println("Você descobre um artefato antigo e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void meeseeksHelp(int forcaAdiconal) {
+        System.out.println("Um Meeseeks te ajuda em um momento crucial e você ganha " + forcaAdiconal + " de poder!");
+    }
+
+
+    public void realityCronenberg(int poderPerdido) {
+        System.out.println("Você enfrenta a realidade Cronenberg e perde " + poderPerdido + " de poder!");
+    }
+
+    public void federationCapture(int poderPerdido) {
+        System.out.println("A Federação te captura e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void plutonianScam(int poderPerdido) {
+        System.out.println("Os Plutonianos te enganam e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void toxicDimension(int poderPerdido) {
+        System.out.println("Você é exposto a uma dimensão tóxica e perde " + poderPerdido + " de poder!");
+    }
+    public void interstellarPortalGun(int forcaAdiconal) {
+        System.out.println("Você encontra a Pistola de Portais e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void schwiftyPerformance(int forcaAdiconal) {
+        System.out.println("Você solta o som mais viciante do universo e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void plumbusDiscovery(int forcaAdiconal) {
+        System.out.println("Você descobre o verdadeiro propósito de um Plumbus e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void interdimensionalCableShow(int forcaAdiconal) {
+        System.out.println("Você participa de um show de TV interdimensional e ganha " + forcaAdiconal + " de poder!");
+    }
+
+
+    public void squanchyBetrayal(int poderPerdido) {
+        System.out.println("Squanchy te trai e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void meeseeksFailure(int poderPerdido) {
+        System.out.println("Um Meeseeks falha em te ajudar e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void federationInterrogation(int poderPerdido) {
+        System.out.println("A Federação te interroga e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void plumbusMisuse(int poderPerdido) {
+        System.out.println("Você usa um Plumbus de forma incorreta e perde " + poderPerdido + " de poder!");
+    }
+
+
+    public void intergalacticPartyInvite(int forcaAdiconal) {
+        System.out.println("Você é convidado para a festa intergaláctica do Birdperson e ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void portalGunUpgrade(int forcaAdiconal) {
+        System.out.println("Sua Pistola de Portais é atualizada e você ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void interdimensionalCouncilFavor(int forcaAdiconal) {
+        System.out.println("O Conselho Interdimensional te favorece e você ganha " + forcaAdiconal + " de poder!");
+    }
+
+    public void mortyEmpowerment(int forcaAdiconal) {
+        System.out.println("Morty te inspira e você ganha " + forcaAdiconal + " de poder!");
+    }
+
+
+    public void spaceBethAttack(int poderPerdido) {
+        System.out.println("A Beth do espaço te ataca e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void dimensionCronenberg(int poderPerdido) {
+        System.out.println("Você enfrenta uma dimensão Cronenberg e perde " + poderPerdido + " de poder!");
+    }
+
+    public void federationInvasion(int poderPerdido) {
+        System.out.println("A Federação invade seu planeta e você perde " + poderPerdido + " de poder!");
+    }
+
+    public void meeseeksRebellion(int poderPerdido) {
+        System.out.println("Os Meeseeks se rebelam e você perde " + poderPerdido + " de poder!");
+    }
+
+}
